2e80bd908990d932d3cbf75ec2f65ddd096334e3
i.dronnikov <idronnikov@atypicaldigital.com>
2023-03-31 13:52:50 +0300

    v

--- core/src/main/java/com/aem/geeks/core/services/impl/ModifyContentService.java
+++ core/src/main/java/com/aem/geeks/core/services/impl/ModifyContentService.java
@@ -6,10 +6,13 @@ import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import org.apache.sling.api.resource.*;
+import org.apache.sling.query.SlingQuery;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import java.util.Iterator;
+
+import static org.apache.sling.query.SlingQuery.$;
 
 @Component(service = ContentService.class, immediate = true)
 public class ModifyContentService implements ContentService {
